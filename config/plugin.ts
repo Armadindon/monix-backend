7860fd55e91c4ee94cd4c9eb987d85e16d4abe80
Armadindon <baptisteperrin77@gmail.com>
2022-09-24 10:49:06 +0300

    AJout mail

--- config/plugin.ts
+++ config/plugin.ts
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = ({ env }) => ({
   upload: {
     config: {
       providerOptions: {
@@ -6,4 +6,17 @@ module.exports = {
       },
     },
   },
-};
+  email: {
+    provider: "smtp",
+    providerOptions: {
+      host: env("SMTP_SERVER"), //SMTP Host
+      port: env("SMTP_PORT"), //SMTP Port
+      secure: true,
+      username: env("MAIL_ADDRESS"),
+      password: env("MAIL_PASSWORD"),
+      rejectUnauthorized: true,
+      requireTLS: true,
+      connectionTimeout: 1,
+    },
+  },
+});
